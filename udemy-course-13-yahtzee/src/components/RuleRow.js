5481d0ccca71eb294bee25ab3d19a71f32119b77
palesmariann <mariann.pales@gmail.com>
2022-01-27 15:05:59 +0100

    preventing score reuse fix

--- udemy-course-13-yahtzee/src/components/RuleRow.js
+++ udemy-course-13-yahtzee/src/components/RuleRow.js
@@ -3,8 +3,12 @@ import '../css/RuleRow.css'
 
 class RuleRow extends Component {
   render() {
+    const disabled = this.props.score !== undefined;
     return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
+      <tr
+          className={`RuleRow RuleRow-${disabled ? 'disabled' : 'active' }`}
+          onClick={disabled ? null : this.props.doScore}
+      >
         <td className="RuleRow-name">{this.props.name}</td>
         <td className="RuleRow-score">{this.props.score}</td>
       </tr>
