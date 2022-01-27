1180f61bcb8b38408f2cd5b5080a3a276f73eaa1
palesmariann <mariann.pales@gmail.com>
2022-01-27 13:19:05 +0100

    lock dice fix

--- udemy-course-13-yahtzee/src/components/Die.js
+++ udemy-course-13-yahtzee/src/components/Die.js
@@ -2,12 +2,18 @@ import React, { Component } from "react";
 import "../css/Die.css";
 
 class Die extends Component {
+  constructor(props) {
+    super(props);
+  }
+  handleClick = () => {
+    this.props.handleClick(this.props.idx)
+  }
   render() {
     return (
       <button
         className={"Die"}
         style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={this.props.handleClick}
+        onClick={this.handleClick}
       >
         {this.props.val}
       </button>
