e10a373d54439c338b6d235c8546fc328b8c955a
palesmariann <mariann.pales@gmail.com>
2022-01-27 16:14:07 +0100

    dice change to icons

--- udemy-course-13-yahtzee/src/components/Die.js
+++ udemy-course-13-yahtzee/src/components/Die.js
@@ -1,22 +1,28 @@
 import React, { Component } from "react";
 import "../css/Die.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix} from "@fortawesome/free-solid-svg-icons";
+
 
 class Die extends Component {
+  static icons = [faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix]
   constructor(props) {
     super(props);
   }
   handleClick = () => {
     this.props.handleClick(this.props.idx)
   }
+  selectIcons = (number) => {
+    return Die.icons[number - 1]
+  }
   render() {
+    let dieClass = this.props.locked ? 'Die-locked' : 'Die';
     return (
-      <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={this.handleClick}
-      >
-        {this.props.val}
-      </button>
+        <div className="" onClick={this.handleClick}>
+          {this.props.val &&
+              <FontAwesomeIcon className={dieClass} icon={this.selectIcons(this.props.val)} size="5x" color="white" />
+          }
+        </div>
     );
   }
 }
