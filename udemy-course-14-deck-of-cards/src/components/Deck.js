726fd436c0e3d0f90b62bc64997f5471dcd71b36
palesmariann <mariann.pales@gmail.com>
2022-01-30 13:13:55 +0100

    display cards

--- udemy-course-14-deck-of-cards/src/components/Deck.js
+++ udemy-course-14-deck-of-cards/src/components/Deck.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
 import axios from "axios";
+import Card from "./Card";
 
+const API_BASE_URL = 'https://deckofcardsapi.com/api/deck'
 class Deck extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            deck: null
+            deck: null,
+            drawn: []
         }
+        this.handleClick = this.handleClick.bind(this)
     }
     async componentDidMount() {
-        const url = 'https://deckofcardsapi.com/api/deck/new/shuffle/'
+        const url = `${API_BASE_URL}/new/shuffle/`
         let deck = await axios.get(url)
         this.setState({deck: deck.data})
                 //deck_id: "b9zw50qku2ww"
@@ -17,12 +21,37 @@ class Deck extends Component {
                 // shuffled: true
                 // success: true
     }
-
+    async handleClick() {
+        const url = `${API_BASE_URL}/${this.state.deck.deck_id}/draw/`;
+        try {
+            let res = await axios.get(url)
+            if (!res.data.success) {
+                throw new Error('No card remaining')
+            }
+            let card = res.data.cards[0]
+            this.setState(st => ({
+                drawn: [
+                    ...st.drawn,
+                    {
+                        id: card.code,
+                        image: card.image
+                    }
+                ]
+            }))
+        } catch (err) {
+            alert(err)
+        }
+    }
     render() {
         return (
             <div>
                 deck id:
                 {this.state.deck && this.state.deck.deck_id}
+                <button onClick={this.handleClick}>Draw card</button>
+                { this.state.drawn && this.state.drawn.map(card => {
+                    return (<Card key={card.id} src={card.image} />)
+                    })
+                }
             </div>
         );
     }
