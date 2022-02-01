9bc07e830e2e85e3c47aa6779869564bf4d8c7e7
palesmariann <mariann.pales@gmail.com>
2022-02-01 21:25:46 +0100

    styling, emojis

--- udemy-course-15-dad-jokes/src/components/Joke.js
+++ udemy-course-15-dad-jokes/src/components/Joke.js
@@ -3,16 +3,36 @@ import '../css/Joke.css'
 import Vote from "./Vote";
 
 class Joke extends Component {
+    getEmoji() {
+        if (this.props.votes >= 15) {
+            return "em em-rolling_on_the_floor_laughing";
+        } else if (this.props.votes >= 12) {
+            return "em em-laughing";
+        } else if (this.props.votes >= 9) {
+            return "em em-smiley";
+        } else if (this.props.votes >= 6) {
+            return "em em-slightly_smiling_face";
+        } else if (this.props.votes >= 3) {
+            return "em em-neutral_face";
+        } else if (this.props.votes >= 0) {
+            return "em em-confused";
+        } else {
+            return "em em-angry";
+        }
+    }
     render() {
         return (
-            <div>
+            <div className="Joke">
                 <Vote
                     value={this.props.votes}
                     id={this.props.id}
                     upvote={this.props.upvote}
                     downvote={this.props.downvote}
                 />
-                <p>{this.props.joke}</p>
+                <div className='Joke-text'>{this.props.joke}</div>
+                <div className='Joke-smiley'>
+                    <i className={this.getEmoji()} />
+                </div>
             </div>
         );
     }
--- udemy-course-15-dad-jokes/src/components/Vote.js
+++ udemy-course-15-dad-jokes/src/components/Vote.js
@@ -1,10 +1,28 @@
 import React, {Component} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons'
+import '../css/Vote.css'
 
 class Vote extends Component {
     constructor(props) {
         super(props);
+    }
+    getColor() {
+        if (this.props.votes >= 15) {
+            return "#4CAF50";
+        } else if (this.props.votes >= 12) {
+            return "#8BC34A";
+        } else if (this.props.votes >= 9) {
+            return "#CDDC39";
+        } else if (this.props.votes >= 6) {
+            return "#FFEB3B";
+        } else if (this.props.votes >= 3) {
+            return "#FFC107";
+        } else if (this.props.votes >= 0) {
+            return "#FF9800";
+        } else {
+            return "#f44336";
+        }
     }
     handleUpvote = () => {
         this.props.upvote()
@@ -14,9 +32,11 @@ class Vote extends Component {
     }
     render() {
         return (
-            <div>
+            <div className='Joke-buttons'>
                 <FontAwesomeIcon onClick={this.handleUpvote} icon={faArrowUp}/>
-                {this.props.value}
+                <span className='Joke-votes' style={{ borderColor: this.getColor() }}>
+                    {this.props.value}
+                </span>
                 <FontAwesomeIcon onClick={this.handleDownvote} icon={faArrowDown}/>
             </div>
         );
